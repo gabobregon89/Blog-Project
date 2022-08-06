231c4b6b774778fbdc033e9d056ccd2dede4fb58
gabobregon89 <gab.obregon89@gmail.com>
2022-08-06 03:20:30 -0300

    Creando estructura

---
+++ src/main/java/com/informatorio/BlogPorject/BlogPorjectApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.BlogPorject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPorjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPorjectApplication.class, args);
+	}
+
+}
